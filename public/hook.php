b0eccfc5dd1806a48ddda69d40cd7c1ab8d2cd86
Lukas Staab <l.staab@posteo.de>
2021-12-12 21:08:44 +0100

    fixed Paths

--- public/hook.php
+++ public/hook.php
@@ -19,12 +19,11 @@ try {
     TelegramLog::initialize($logger);
     $telegram = new Longman\TelegramBot\Telegram($_ENV['TG_BOT_SECRET'], $_ENV['TG_BOT_USERNAME']);
     $telegram->addCommandsPaths([
-        ROOT . 'lib/tg/UserCommands/',
-        ROOT . 'lib/tg/AdminCommands/',
-        ROOT . 'lib/tg/SystemCommands/'
+        ROOT . '/lib/tg/UserCommands/',
+        ROOT . '/lib/tg/AdminCommands/',
+        ROOT . '/lib/tg/SystemCommands/'
     ]);
     // Handle telegram webhook request
-    $logger->debug('Incoming msg');
     $telegram->handle();
 } catch (Exception $e) {
     // Silence is golden!
